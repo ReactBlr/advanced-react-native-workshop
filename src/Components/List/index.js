0b5c0b227e95cdfd68efbad797ba6b2a35fc60be
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2018-08-30 11:41:14 +0530

    more flatlist

--- src/Components/List/index.js
+++ src/Components/List/index.js
@@ -18,6 +18,7 @@ export default class List extends Component {
     this.state = {
       loading: false,
       data: [],
+      filteredData: [],
       page: 1,
       seed: 1,
       error: null,
@@ -26,23 +27,26 @@ export default class List extends Component {
   }
 
   componentDidMount() {
-    this.makeRemoteRequest();
+    this.setState({ loading: true }, () => this.makeRemoteRequest());
   }
 
   makeRemoteRequest = async () => {
     const { page, seed } = this.state;
     const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
-    this.setState({ loading: true });
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
-      this.setState({
-        data: page === 1 ? data.results : [...this.state.data, ...data.results],
-        error: data.error || null,
-        loading: false,
-        refreshing: false
-      });
+      console.log(this.state);
+      this.setState(
+        {
+          data:
+            page === 1 ? data.results : [...this.state.data, ...data.results],
+          error: data.error || null,
+          loading: false,
+          refreshing: false
+        },
+        () => this.searchFilterFunction("")
+      );
     } catch (error) {
       this.setState({ error, loading: false });
     }
@@ -75,41 +79,74 @@ export default class List extends Component {
           }}
           underlineColorAndroid={"transparent"}
           placeholder={"Search"}
+          onChangeText={text => this.searchFilterFunction(text)}
         />
       </View>
     );
   };
 
   renderFooter = () => {
-    if (!this.state.loading) return null;
-    return (
-      <View
-        style={{
-          paddingVertical: 20,
-          borderTopWidth: 1,
-          borderColor: "#CED0CE"
-        }}
-      >
-        <ActivityIndicator animating size={"large"} />
-      </View>
+    if (this.state.loading && this.state.data.length !== 0) {
+      return (
+        <View
+          style={{
+            paddingVertical: 20,
+            borderTopWidth: 1,
+            borderColor: "#CED0CE",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <ActivityIndicator animating size={"large"} />
+        </View>
+      );
+    }
+    return null;
+  };
+
+  searchFilterFunction = text => {
+    const newData = this.state.data.filter(item => {
+      const itemData = `${item.name.first.toUpperCase()} ${item.name.last.toUpperCase()}`;
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({ filteredData: newData });
+  };
+
+  handleRefresh = () => {
+    this.setState({ refreshing: true, seed: Math.random() }, () =>
+      this.makeRemoteRequest()
+    );
+  };
+
+  handleOnEndReached = () => {
+    console.log("inside on end reached");
+    this.setState(
+      (state, props) => {
+        return { loading: true, page: state.page + 1 };
+      },
+      () => this.makeRemoteRequest()
     );
   };
 
   render() {
     return (
       <FlatList
-        data={this.state.data}
+        data={this.state.filteredData}
         renderItem={({ item }) => {
-          console.log(item, "🎉🎉🎉");
           return (
-            <View style={{ flexDirection: "row", flex: 1 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                flex: 1
+              }}
+            >
               <Image
-                width={25}
-                height={25}
+                style={{ width: 40, height: 40, borderRadius: 20, margin: 5 }}
                 source={{ uri: item.picture.thumbnail }}
                 resizeMethod={"resize"}
               />
-              <View>
+              <View style={{ justifyContent: "center", marginLeft: 5 }}>
                 <Text>{`${item.name.first} ${item.name.last}`}</Text>
                 <Text>{item.email}</Text>
               </View>
@@ -120,7 +157,12 @@ export default class List extends Component {
         ItemSeparatorComponent={this.renderSeparator}
         ListHeaderComponent={this.renderHeader}
         ListFooterComponent={this.renderFooter}
-        removeClippedSubviews={true}
+        ListEmptyComponent={<ActivityIndicator animating size={"large"} />}
+        extraData={this.state.filteredData}
+        refreshing={this.state.refreshing}
+        onRefresh={this.handleRefresh}
+        onEndReachedThreshold={0.9}
+        onEndReached={this.handleOnEndReached}
       />
     );
   }
