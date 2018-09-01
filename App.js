ed3732222138ffd919601e515de98744e7c89201
kiran <kiran@neostack.com>
2018-09-01 13:16:47 +0530

    Switch between Storybook and App

--- App.js
+++ App.js
@@ -1,25 +1,5 @@
-// import React from "react";
-// import { StyleSheet, Text, View } from "react-native";
+import App from "./components/App";
+import StorybookUI from "./storybook";
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>!!Open up App.js to start working on your app!</Text>
-//         <Text>Changes you make will automatically reload.</Text>
-//         <Text>Shake your phone to open the developer menu.</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center"
-//   }
-// });
-
-export default from "./storybook";
+const UI = process.env.REACT_NATIVE_STORYBOOK ? StorybookUI : App;
+export default UI;
